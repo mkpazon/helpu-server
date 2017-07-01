a8a2ec188201f96ddf104936618697ffac2e868a
Mark Pazon <mkpazon@gmail.com>
2017-07-02 06:24:06 +0800

    Created temporary parse class for notification

--- cloud/main.js
+++ cloud/main.js
@@ -16,23 +16,22 @@ var ClassConstants = {
     }
 }
 
-Parse.Cloud.define("sendNotification", function(request, response) {
-    console.log("sendNotification");
-    Parse.Cloud.useMasterKey();
-     var installationQuery = new Parse.Query(Parse.Installation);
-        installationQuery.equalTo("objectId", "Oc3pXVrtFn");
-        var object = {
-            type: "scheduleConfirmed",
-            message: "Your booking has been confirmed"
-        };
+Parse.Cloud.afterSave("TempNotif", function(request, response) {
+    console.log("\n######TempNotif.afterSave()");
+    var installationQuery = new Parse.Query(Parse.Installation);
+    installationQuery.equalTo("objectId", "Oc3pXVrtFn");
+    var object = {
+        type: "scheduleConfirmed",
+        message: "Your booking has been confirmed"
+    };
 
-        installationQuery.find(function(results) {
-            console.log("installation query results:"+JSON.stringify(results));
-            response.success(results);
-        }, function(error) {
-            console.log("Got an error " + error.code + " : " + error.message);
-            response.error("Failed to send push app_update");
-        });
+    installationQuery.find(function(results) {
+        console.log("installation query results:"+JSON.stringify(results));
+        response.success(results);
+    }, function(error) {
+        console.log("Got an error " + error.code + " : " + error.message);
+        response.error("Failed to send push app_update");
+    });
 
         // Parse.Push.send({
         //     where: installationQuery,
@@ -44,4 +43,28 @@ Parse.Cloud.define("sendNotification", function(request, response) {
         //     console.log("Got an error " + error.code + " : " + error.message);
         //     response.success();
         // });
+
+});
+
+
+Parse.Cloud.define("sendNotification", function(request, response) {
+    console.log("sendNotification");
+
+    // Simple syntax to create a new subclass of Parse.Object.
+    var TempNotif = Parse.Object.extend("TempNotif");
+
+    // Create a new instance of that class.
+    var tempNotif = new TempNotif();
+
+    tempNotif.save(null, {
+      success: function(tempNotif) {
+        // Execute any logic that should take place after the object is saved.
+        alert('New object created with objectId: ' + tempNotif.id);
+      },
+      error: function(tempNotif, error) {
+        // Execute any logic that should take place if the save fails.
+        // error is a Parse.Error with an error code and message.
+        alert('Failed to create new object, with error code: ' + error.message);
+      }
+    });
 });
